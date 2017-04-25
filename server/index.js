bed1ddf8374e5103478e74dce7bbd072c6df44b2
DenDos <cotoha_92@mail.ru>
2017-04-25 12:29:27 +0200

    test

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
-import config from './config'
-import mongoose from 'mongoose'
+// import config from './config'
+// import mongoose from 'mongoose'
 
-mongoose.Promise = global.Promise;
-mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'))
-mongoose.set('debug', true);
+// mongoose.Promise = global.Promise;
+// mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'))
+// mongoose.set('debug', true);
 
-require('./services')
+// require('./services')
+
+
+import express from "express"
+ 
+ 
+var app = express();
+ 
+app.get("/", (req, res) => {
+    res.json({hello: 'world'});
+});
+ 
+ 
+var port = process.env.PORT || 3000;
+ 
+var server = app.listen(port, () => {
+    console.log('Service started on port :' + port);
+});
